92ea5828008780c485ce94f73a07f128aa74164f
Indra Mahesa <indramahesa128@gmail.com>
2023-10-15 22:52:21 +0700

    Initial Commit

---
+++ tp5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tp5
+//
+//  Created by Indra Mahesa on 15/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
